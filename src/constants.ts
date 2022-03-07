102c7b1afd7a2e74a77f4a9c0d6f07d9c2dc40e2
Nathan Lin <nlin.wei.an@gmail.com>
2022-03-07 14:41:54 -0800

    added Currency constant and type

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+import { Currency } from "./types";
+
+export const CURRENCIES: Currency[] = [
+    {
+        name: "Ethereum",
+        id: "ETH"
+    },
+    {
+        name: "Solana",
+        id: "SOL"
+    },
+    {
+        name: "Polygon",
+        id: "MATIC"
+    },
+    {
+        name: "Avalanche",
+        id: "AVAX"
+    }
+]

686a11f683b345c18b0d8e7fcbb2e0a629055e0c
Nathan Lin <nlin.wei.an@gmail.com>
2022-03-07 14:59:06 -0800

    Update CurrencyRateDisplay
    The following changes were made in this commit:
    - Added 4 component states (amount, currencyId, priceValidityTime, and currencyPriceInUSD)
    - Added 2 effects, one effect to maintain a countdown timer and another to retrieve the price data from Coinbase
    - Added component elements to stack header text with the currency form and price summary

--- src/components/CurrencyRateDisplay.tsx
+++ src/components/CurrencyRateDisplay.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import {
     Container,
     VStack,
@@ -6,12 +7,47 @@ import {
     Text,
     Spinner,
     Flex,
+    usePrevious
 } from "@chakra-ui/react";
 import CurrencyForm from "./CurrencyForm";
+import { CURRENCIES } from "../constants";
 import PriceSummary from "./PriceSummary";
+import { getCurrencyPriceInUSD } from "../utils";
 
+const PRICE_REFRESH_TIME_IN_SECONDS = 30
 
 const CurrencyRateDisplay: React.FunctionComponent<{}> = (props) => {
+    // amount is set as a string in order to make working with the Chakra-UI 
+    // NumberInput component easier
+    const [amount, setAmount] = useState<string>("0")
+    const [currencyId, setCurrencyId] = useState<string>(CURRENCIES[0].id)
+    // If the time is 0 then we want to requote
+    const [priceValidityTime, setPriceValidityTime] = useState<number>(0)
+    // Set the initial state to -1 to indicate we haven't retrieved the price in USD yet
+    const [currencyPriceInUSD, setCurrencyPriceInUSD] = useState<number>(-1)
+
+    const prevCurrencyId = usePrevious(currencyId)
+
+    // This hook will create a count down timer on component mount
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setPriceValidityTime((prevTime) => (prevTime - 1) >= 0 ? prevTime - 1 : 0)
+        }, 1000, priceValidityTime)
+
+        // Clean up the interval on component unmount
+        return () => clearInterval(intervalId)
+    }, [])
+
+    useEffect(() => {
+        // if priceValidityTime is zero or the currency changed
+        if (priceValidityTime === 0 || prevCurrencyId !== currencyId) {
+            getCurrencyPriceInUSD(currencyId).then(d => {
+                setCurrencyPriceInUSD(() => parseFloat(d.data.amount))
+                setPriceValidityTime(() => PRICE_REFRESH_TIME_IN_SECONDS)
+            })
+        }
+    }, [priceValidityTime, currencyId, prevCurrencyId])
+
     return (
         <Container
             maxW="440px"
@@ -21,15 +57,24 @@ const CurrencyRateDisplay: React.FunctionComponent<{}> = (props) => {
             border="1px"
             borderColor="gray.200"
             borderRadius={12}>
-            {true ? (
+            {currencyPriceInUSD >= 0 ? (
                 <VStack w="full" h="full" spacing={10} padding={10}>
                     <VStack spacing={1} w="full" alignItems="flex-start">
                         <Heading size="lg">Currency Rate</Heading>
                         <Text>Select a currency and amount to convert to USD.</Text>
                         <Divider />
                     </VStack>
-                    <CurrencyForm />
-                    <PriceSummary />
+                    <CurrencyForm
+                        amount={amount}
+                        currencyId={currencyId}
+                        onCurrencyChange={(v) => setCurrencyId(v)}
+                        onAmountChange={(v) => setAmount(v)} />
+                    <PriceSummary
+                        currencyId={currencyId}
+                        priceValidityTime={priceValidityTime}
+                        currencyPriceInUSD={currencyPriceInUSD}
+                        totalCostInUSD={amount ? parseFloat(amount) : 0}
+                    />
                 </VStack>
             ) : (
                 <Flex h="full" alignItems="center" justifyContent="center">

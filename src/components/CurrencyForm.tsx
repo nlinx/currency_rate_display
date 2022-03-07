9ae9e0e8db65f692900f2191b6ae6b6808ae6873
Nathan Lin <nlin.wei.an@gmail.com>
2022-03-07 14:42:38 -0800

    Added props and UI for CurrencyForm and PriceSummary

--- src/components/CurrencyForm.tsx
+++ src/components/CurrencyForm.tsx
@@ -1,8 +1,62 @@
-type Props = {}
+import {
+    VStack,
+    FormControl,
+    FormLabel,
+    Select,
+    Flex,
+    Text,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper
+} from "@chakra-ui/react";
+import { CURRENCIES } from "../constants";
+
+type Props = {
+    currencyId: string;
+    amount: string;
+    onCurrencyChange: (value: string) => void;
+    onAmountChange: (value: string) => void;
+}
 
 const CurrencyForm: React.FunctionComponent<Props> = (props) => {
     return (
-        <></>
+        <VStack spacing={4} w="full">
+            <FormControl>
+                <FormLabel>Currency</FormLabel>
+                <Select
+                    backgroundColor="gray.100"
+                    value={props.currencyId}
+                    onChange={(e) => {
+                        props.onCurrencyChange(e.target.value)
+                    }}>
+                    {CURRENCIES.map((currency, i) => (
+                        <option key={i} value={currency.id}>{currency.name}</option>
+                    ))}
+                </Select>
+            </FormControl>
+            <FormControl>
+                <Flex justifyContent="space-between">
+                    <FormLabel>
+                        <Text>Amount</Text>
+                    </FormLabel>
+                    <Text>{`${props.currencyId} / USD`}</Text>
+                </Flex>
+                <NumberInput
+                    value={props.amount}
+                    min={0}
+                    onChange={(amount) => {
+                        props.onAmountChange(amount)
+                    }}>
+                    <NumberInputField backgroundColor="gray.100" />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+            </FormControl>
+        </VStack>
     );
 }
 
--- src/components/PriceSummary.tsx
+++ src/components/PriceSummary.tsx
@@ -1,8 +1,37 @@
-type Props = {}
+import { VStack, Flex, Text } from "@chakra-ui/react";
+
+type Props = {
+    currencyId: string;
+    priceValidityTime: number;
+    currencyPriceInUSD: number;
+    totalCostInUSD: number;
+}
 
 const PriceSummary: React.FunctionComponent<Props> = (props) => {
+    const totalCurrency = props.totalCostInUSD / props.currencyPriceInUSD
     return (
-        <></>
+        <VStack spacing={1} w="full" h="full">
+            <Flex w="full" justifyContent="space-between">
+                <Text fontWeight="semibold">Summary</Text>
+                {props.priceValidityTime === 0 ? (
+                    <Text>Re-quoting</Text>
+                ) : (
+                    <Text>{`Valid for ${props.priceValidityTime}s`}</Text>
+                )}
+            </Flex>
+            <Flex w="full" justifyContent="space-between">
+                <Text fontWeight="semibold">{`${props.currencyId} Price`}</Text>
+                <Text>{`$${props.currencyPriceInUSD.toFixed(2)}`}</Text>
+            </Flex>
+            <Flex w="full" justifyContent="space-between">
+                <Text fontWeight="semibold">You get</Text>
+                <Text>{`${totalCurrency.toFixed(5)} ${props.currencyId}`}</Text>
+            </Flex>
+            <Flex w="full" justifyContent="space-between">
+                <Text fontWeight="semibold">Total</Text>
+                <Text>{`$${props.totalCostInUSD.toFixed(2)}`}</Text>
+            </Flex>
+        </VStack>
     );
 }
 
